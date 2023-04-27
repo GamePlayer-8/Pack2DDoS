1f846c560b001f464d7f912ef3494f2c410bf2e9
GamePlayer-8 <gameplayer2019pl@tutamail.com>
2023-04-27 12:28:24 +0200

    Execute hammer with runpy.

--- standalone.py
+++ standalone.py
@@ -5,4 +5,5 @@ os.chdir(getattr(sys, '_MEIPASS', os.path.dirname(os.path.abspath(__file__))))
 sys.path.append(os.getcwd())
 PYBIN = sys.executable
 
-import hammer
+import runpy
+result = runpy._run_module_as_main("hammer")
