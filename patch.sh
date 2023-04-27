6b90e8cbc41e60666dcd7b44083b20a328dbe21c
GamePlayer-8 <gameplayer2019pl@tutamail.com>
2023-04-27 13:21:18 +0200

    Fix error.

--- patch.sh
+++ patch.sh
@@ -9,6 +9,7 @@ cd $SCRIPT_PATH
 git clone --recursive https://github.com/cyweb/hammer DDoS
 
 sed -i 's/time.sleep(.1)/###/g' DDoS/hammer.py
+sed -i 's/except:/except: pass/g' DDoS/hammer.py
 
 cp standalone.py DDoS/
 
