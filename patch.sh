cb7662c9076de887fbe58f118be91cb55ddd2eff
GamePlayer-8 <gameplayer2019pl@tutamail.com>
2023-04-27 11:46:22 +0200

    Check match PyRoxy.

--- patch.sh
+++ patch.sh
@@ -8,6 +8,8 @@ cd $SCRIPT_PATH
 
 git clone --recursive https://github.com/MatrixTM/MHDDoS DDoS
 
+sed -i 's/git+https://github.com/MHProDev/PyRoxy.git/PyRoxy/g' DDoS/requirements.txt
+
 cp DDoS/requirements.txt .
 cp standalone.py DDoS/
 
